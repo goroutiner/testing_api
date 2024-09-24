8649d5a0d68729cedd17aeb5494209e575eedb5f
Alexey <bakovin26@list.ru>
2024-09-24 19:20:19 +0300

    first try

--- test-api_test.go
+++ test-api_test.go
@@ -1,58 +1,50 @@
 package main
 
 import (
-    "net/http"
-    "net/http/httptest"
-    "strconv"
-    "strings"
-    "testing"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"testing_api/api"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-var cafeList = map[string][]string{
-    "moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
+// Запрос сформирован корректно, сервис возвращает код ответа 200 и тело ответа не пустое.
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(api.MainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.NotNil(t, responseRecorder.Body)
 }
 
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-    countStr := req.URL.Query().Get("count")
-    if countStr == "" {
-        w.WriteHeader(http.StatusBadRequest)
-        w.Write([]byte("count missing"))
-        return
-    }
+// Город, который передаётся в параметре city, не поддерживается. Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа.
+func TestInvalidCity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=kazan", nil)
 
-    count, err := strconv.Atoi(countStr)
-    if err != nil {
-        w.WriteHeader(http.StatusBadRequest)
-        w.Write([]byte("wrong count value"))
-        return
-    }
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(api.MainHandle)
+	handler.ServeHTTP(responseRecorder, req)
 
-    city := req.URL.Query().Get("city")
-
-    cafe, ok := cafeList[city]
-    if !ok {
-        w.WriteHeader(http.StatusBadRequest)
-        w.Write([]byte("wrong city value"))
-        return
-    }
-
-    if count > len(cafe) {
-        count = len(cafe)
-    }
-
-    answer := strings.Join(cafe[:count], ",")
-
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte(answer))
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+	assert.Equal(t, "wrong city value", responseRecorder.Body.String())
 }
 
+// Если в параметре count указано больше, чем есть всего, должны вернуться все доступные кафе.
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-    totalCount := 4
-    req := ... // здесь нужно создать запрос к сервису
+	totalCount := 4
 
-    responseRecorder := httptest.NewRecorder()
-    handler := http.HandlerFunc(mainHandle)
-    handler.ServeHTTP(responseRecorder, req)
+	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
 
-    // здесь нужно добавить необходимые проверки
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(api.MainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	stringWithCafe := responseRecorder.Body.String()
+	listOfCafe := strings.Split(stringWithCafe, ",")
+	assert.Len(t, listOfCafe, totalCount)
 }
